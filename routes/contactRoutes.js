44c35bc074332b6b255fc138022b36bbfd2396b2
Jaiswarpooja <89985685+Jaiswarpooja@users.noreply.github.com>
2023-08-09 14:57:41 +0530

    Add files via upload

---
+++ routes/contactRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const { getContact,
+        createContact,
+        getContacts,
+        updateContact,
+        deleteContact
+       } = require("../controllers/contactController");
+const validateToken = require("../middleware/validateTokenHandler");
+
+router.use(validateToken);
+       
+router.route("/").get(getContacts);
+
+router.route("/").post(createContact);
+
+router.route("/:id").put(updateContact);
+
+router.route("/:id").delete(deleteContact);
+
+router.route("/:id").get(getContact);
+
+module.exports = router;
